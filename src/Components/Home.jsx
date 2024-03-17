9231f48f290bdaa4608078f6a5dd32d1b66edd64
Abdul-Quadri Odunayo Soliu <138295250+uniqtechbro@users.noreply.github.com>
2024-03-17 11:49:39 +0000

    committed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "/images/IMG-20230815-WA0054.jpg";
+import image from "../images/IMG-20230815.jpg";
 
 const imageAltText = "A Product Designer working with a laptop";
 
